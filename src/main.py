c718d1beb2bf55f064c51b698935e812ed8e6326
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-09-20 16:00:39 +0200

    Rewrite database connection function

---
+++ src/main.py
@@ -0,0 +1,33 @@
+import os
+from dotenv import load_dotenv
+import psycopg2
+
+
+def connect():
+    connection = None
+    try:
+        print("Connecting to the postgreSQL ...")
+
+        load_dotenv()
+        database_user = os.getenv("DATABASE_USER")
+        database_password = os.getenv("DATABASE_PASSWORD")
+
+        connection = psycopg2.connect(
+            host="localhost",
+            database="facethefacts",
+            user=database_user,
+            password=database_password,
+        )
+        print("Connected to the facethefacts database")
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+
+    finally:
+        if connection is not None:
+            print("Closed the database")
+            connection.close()
+
+
+if __name__ == "__main__":
+    connect()
