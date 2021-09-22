dbe6507bf89e015741f5202ffbe431df1ff29296
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-09-22 23:25:47 +0200

    Add insert_data function

--- src/party.py
+++ src/party.py
@@ -1,6 +1,8 @@
 from connection import connect
 import psycopg2
 
+from fetch import party_fetch
+
 conn = connect()
 cur = conn.cursor()
 
@@ -24,6 +26,29 @@ class Party:
         finally:
             conn.close()
 
+    def insert_data(self):
+        parties = party_fetch()
+        try:
+            for party in parties:
+                cur.execute(
+                    "INSERT INTO party (id, entity_type, label, api_url, full_name, short_name) VALUES(%s,%s,%s,%s,%s,%s)",
+                    (
+                        party["id"],
+                        party["entity_type"],
+                        party["label"],
+                        party["api_url"],
+                        party["full_name"],
+                        party["short_name"],
+                    ),
+                )
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
+        finally:
+            conn.commit()
+            cur.close()
+            conn.close()
+
 
 party = Party()
-party.create_table()
+# party.create_table()
+print(party.insert_data())
