a3befdf2b589d27879aaf521dfa967ea7692c077
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-09-23 11:57:51 +0200

    Add comments

--- src/query.py
+++ src/query.py
@@ -13,12 +13,14 @@ class Query:
         return self.cur.execute(query, *vars)
 
     def cursor_close(self):
+        print("Cursor is closed")
         return self.cur.close()
 
     def connection_commit(self):
         return self.conn.commit()
 
     def connection_close(self):
+        print("Connection is closed")
         return self.conn.close()
 
     def sql_command_execution(self, query, *vars):
