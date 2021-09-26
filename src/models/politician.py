aa7e2350b955e8419a4fab34af157996da10bfe0
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-09-26 16:56:40 +0200

    Add insert_data funciton

--- src/models/politician.py
+++ src/models/politician.py
@@ -84,6 +84,105 @@ class Politician:
 
         return self.new_query.sql_command_execution(sql_command)
 
+    def insert_data(self):
+        politicians = politician_fetch()
+        for politician in politicians:
+            sql_string = """INSERT INTO {} 
+          (
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {}
+          ) VALUES (
+              %s,
+            %s,
+            %s,
+            %s,
+            %s,
+            %s,
+            %s,
+            %s,
+            %s,
+            %s,
+            %s,
+            %s,
+            %s,
+            %s,
+            %s,
+            %s,
+            %s,
+            %s,
+            %s,
+            %s,
+            %s
+            
+          )""".format(
+                self.table_name,
+                columns[0],
+                columns[1],
+                columns[2],
+                columns[3],
+                columns[4],
+                columns[5],
+                columns[6],
+                columns[7],
+                columns[8],
+                columns[9],
+                columns[10],
+                columns[11],
+                columns[12],
+                columns[13],
+                columns[14],
+                columns[15],
+                columns[16],
+                columns[17],
+                columns[18],
+                columns[19],
+                columns[20],
+            )
+            sql_tuple = (
+                politician[columns[0]],
+                politician[columns[1]],
+                politician[columns[2]],
+                politician[columns[3]],
+                politician[columns[4]],
+                politician[columns[5]],
+                politician[columns[6]],
+                politician[columns[7]],
+                politician[columns[8]],
+                politician[columns[9]],
+                politician["party"]["id"],
+                politician[columns[11]],
+                politician[columns[12]],
+                politician[columns[13]],
+                politician[columns[14]],
+                politician[columns[15]],
+                politician[columns[16]],
+                politician[columns[17]],
+                politician[columns[18]],
+                politician[columns[19]],
+                politician[columns[20]],
+            )
+            self.new_query.sql_command_execution(sql_string, sql_tuple)
+        return
+
     def cursor_close(self):
         return self.new_query.cursor_close()
 
@@ -92,7 +191,7 @@ class Politician:
 
 
 politician = Politician()
-politician.create_table()
-
+# politician.create_table()
+politician.insert_data()
 politician.cursor_close()
 politician.connection_close()
