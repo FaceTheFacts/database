491b5a865bd01a15201e735d0c238691a44448de
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-09-27 13:05:16 +0200

    Set range to 1000

--- src/fetch.py
+++ src/fetch.py
@@ -4,13 +4,16 @@ import json
 
 
 def fetch(entity: str):
-    BASE_URL = "https://www.abgeordnetenwatch.de/api/v2/{entity}?&page={page}"
+    BASE_URL = (
+        "https://www.abgeordnetenwatch.de/api/v2/{entity}?&page={page}&range_end={range_end}"
+    )
     page_number = 0
+    range_end = 1000
     finished = False
     fetched_data_list = []
     while not finished:
         print("")
-        url = BASE_URL.format(entity=entity, page=page_number)
+        url = BASE_URL.format(entity=entity, page=page_number, range_end=range_end)
         response = requests.get(url)
         try:
             response.raise_for_status()
