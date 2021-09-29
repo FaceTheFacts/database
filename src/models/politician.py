fad248222ce35bc109b9a7c5d0e6e85fcab9ae1e
Kevin Gao <k_gao@aol.com>
2021-09-29 14:54:42 +0200

    refactor: clean up sql format tuple

--- src/models/politician.py
+++ src/models/politician.py
@@ -59,28 +59,7 @@ class Politician:
       {} varchar,
       {} varchar
     );""".format(
-            self.table_name,
-            columns[0],
-            columns[1],
-            columns[2],
-            columns[3],
-            columns[4],
-            columns[5],
-            columns[6],
-            columns[7],
-            columns[8],
-            columns[9],
-            columns[10],
-            columns[11],
-            columns[12],
-            columns[13],
-            columns[14],
-            columns[15],
-            columns[16],
-            columns[17],
-            columns[18],
-            columns[19],
-            columns[20],
+            self.table_name, *columns
         )
 
         return self.new_query.sql_command_execution(sql_command)
@@ -91,7 +70,7 @@ class Politician:
         for politician in politicians:
             id = politician.get("id")
             party = politician.get("party")
-            sql_string = """INSERT INTO {} 
+            sql_string = """INSERT INTO {}
             (
                 {},
                 {},
@@ -136,84 +115,23 @@ class Politician:
                     %s,
                     %s,
                     %s
-                    
+
                 ) ON CONFLICT(id) DO NOTHING""".format(
                 self.table_name,
-                columns[0],
-                columns[1],
-                columns[2],
-                columns[3],
-                columns[4],
-                columns[5],
-                columns[6],
-                columns[7],
-                columns[8],
-                columns[9],
-                columns[10],
-                columns[11],
-                columns[12],
-                columns[13],
-                columns[14],
-                columns[15],
-                columns[16],
-                columns[17],
-                columns[18],
-                columns[19],
-                columns[20],
+                *columns,
             )
+
+            politician_values = list(map(lambda k: politician[k], columns))
 
             if party is None:
                 print("ID: No.{id} politician doesn't have a party".format(id=id))
-                no_party_politicians += politician
-                sql_tuple = (
-                    politician[columns[0]],
-                    politician[columns[1]],
-                    politician[columns[2]],
-                    politician[columns[3]],
-                    politician[columns[4]],
-                    politician[columns[5]],
-                    politician[columns[6]],
-                    politician[columns[7]],
-                    politician[columns[8]],
-                    politician[columns[9]],
-                    politician["party"],
-                    politician[columns[11]],
-                    politician[columns[12]],
-                    politician[columns[13]],
-                    politician[columns[14]],
-                    politician[columns[15]],
-                    politician[columns[16]],
-                    politician[columns[17]],
-                    politician[columns[18]],
-                    politician[columns[19]],
-                    politician[columns[20]],
-                )
-                self.new_query.sql_command_execution(sql_string, sql_tuple)
+                politician_values[10] = politician["party"]
             else:
-                sql_tuple_with_id = (
-                    politician[columns[0]],
-                    politician[columns[1]],
-                    politician[columns[2]],
-                    politician[columns[3]],
-                    politician[columns[4]],
-                    politician[columns[5]],
-                    politician[columns[6]],
-                    politician[columns[7]],
-                    politician[columns[8]],
-                    politician[columns[9]],
-                    politician["party"]["id"],
-                    politician[columns[11]],
-                    politician[columns[12]],
-                    politician[columns[13]],
-                    politician[columns[14]],
-                    politician[columns[15]],
-                    politician[columns[16]],
-                    politician[columns[17]],
-                    politician[columns[18]],
-                    politician[columns[19]],
-                    politician[columns[20]],
-                )
-                self.new_query.sql_command_execution(sql_string, sql_tuple_with_id)
+                politician_values[10] = politician["party"]["id"]
+
+            sql_tuple = tuple(politician_values)
+
+            self.new_query.sql_command_execution(sql_string, sql_tuple)
 
         with open("no_party_politicians.json", "w", encoding="utf-8") as file:
             json.dump(no_party_politicians, file, ensure_ascii=False, indent=4)
