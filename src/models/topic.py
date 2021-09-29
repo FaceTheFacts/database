0e98ae17aba064c637b745e40fa579a2b4d06ad5
Richard <richard.kruemmel@code.berlin>
2021-09-29 14:40:55 +0200

    Add comments to explain

--- src/models/topic.py
+++ src/models/topic.py
@@ -23,7 +23,7 @@ class Topic:
     def insert_data(self):
         topics = topic_fetch()
         for topic in topics:
-            """ if topic['description'] != None: """
+            "Run this for creating the table"
             self.new_query.sql_command_execution(
                 "INSERT INTO topic (id, entity_type, label, api_url, abgeordnetenwatch_url, description) VALUES(%s,%s,%s,%s,%s,%s)",
                 (
@@ -35,20 +35,19 @@ class Topic:
                     topic["description"]
                 ),
             )
+            "Run this when you have created the table because parents relates to the topic id. Parent can be undefined, therefore we have to check it."
+            """ if topic["parent"] != None:
+                sql_command = "UPDATE topic SET parent={parent} WHERE id={topic_id}".format(parent=topic["parent"][0]["id"], topic_id=topic["id"]) 
+                self.new_query.sql_command_execution(sql_command)
+            else:
+               sql_command = "UPDATE topic SET parent=NULL WHERE id={topic_id}".format(topic_id=topic["id"]) 
+               self.new_query.sql_command_execution(sql_command)  """
         return
-        """ if topic['description'] == None:
-                self.new_query.sql_command_execution(
-                    "INSERT INTO topic (id, entity_type, label, api_url, abgeordnetenwatch_url, description) VALUES(%s,%s,%s,%s,%s,%s)",
-                    (
-                        topic["id"],
-                        topic["entity_type"],
-                        topic["label"],
-                        topic["api_url"],
-                        topic["abgeordnetenwatch_url"],
-                        None,
-                    ),
-                )
-                return """
+
+    def alter_table(self):
+        sql_command = """ALTER topic
+      ADD COLUMN parent integer REFERENCES topic(id)""" 
+        return self.new_query.sql_command_execution(sql_command)
 
     def cursor_close(self):
         return self.new_query.cursor_close()
@@ -58,7 +57,7 @@ class Topic:
 
 
 topic = Topic()
-topic.create_table()
+topic.alter_table()
 topic.insert_data()
 topic.cursor_close()
 topic.connection_close()
