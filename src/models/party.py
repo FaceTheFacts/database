817918def1d64d1610107d51b2eff48223de66ae
Kevin Gao <k_gao@aol.com>
2021-09-30 23:09:00 +0200

    refactor: enable party to write mutiple rows

--- src/models/party.py
+++ src/models/party.py
@@ -8,32 +8,40 @@ from query import Query
 class Party:
     def __init__(self):
         self.new_query = Query()
+        self.columns = [
+            "id",
+            "entity_type",
+            "label",
+            "api_url",
+            "full_name",
+            "short_name",
+        ]
 
     def create_table(self):
-        sql_command = """CREATE TABLE party (
-      id integer PRIMARY KEY,
-      entity_type varchar,
-      label varchar,
-      api_url varchar,
-      full_name varchar,
-      short_name varchar
-    );"""
+        sql_command = """
+        CREATE TABLE party (
+            id integer PRIMARY KEY,
+            entity_type varchar,
+            label varchar,
+            api_url varchar,
+            full_name varchar,
+            short_name varchar
+        );"""
         return self.new_query.sql_command_execution(sql_command)
 
-    def insert_data(self) -> None:
-        party_list = party_fetch()
-        for party in party_list:
-            self.new_query.sql_command_execution(
-                "INSERT INTO party (id, entity_type, label, api_url, full_name, short_name) VALUES(%s,%s,%s,%s,%s,%s)",
-                (
-                    party["id"],
-                    party["entity_type"],
-                    party["label"],
-                    party["api_url"],
-                    party["full_name"],
-                    party["short_name"],
-                ),
-            )
+    def populate_table(self):
+        api_parties = party_fetch()
+        party_list = []
+
+        for api_party in api_parties:
+            party_tuple = tuple(map(lambda k: api_party[k], self.columns))
+            party_list.append(party_tuple)
+
+        query_str = self.new_query.generate_query_str(party_list)
+        self.new_query.sql_command_execution(
+            "INSERT INTO party (id, entity_type, label, api_url, full_name, short_name) VALUES "
+            + query_str
+        )
 
     def cursor_close(self):
         return self.new_query.cursor_close()
@@ -44,6 +52,6 @@ class Party:
 
 party = Party()
 party.create_table()
-party.insert_data()
+party.populate_table()
 party.cursor_close()
 party.connection_close()
