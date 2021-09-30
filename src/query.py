c2777e60f8641bef34e781358894352c34fff4a7
Kevin Gao <k_gao@aol.com>
2021-09-30 23:08:20 +0200

    feat: add method to Query class for query string generation

--- src/query.py
+++ src/query.py
@@ -1,3 +1,4 @@
+from typing import Any
 from connection import connect
 import psycopg2
 
@@ -18,6 +19,13 @@ class Query:
     def connection_close(self):
         print("Connection is closed")
         return self.conn.close()
+
+    def generate_query_str(self, entities: list[Any]) -> str:
+        placeholder = f'({"".join("%s," for _ in range(len(entities[0])))[:-1]})'
+        query_str = ",".join(
+            self.cur.mogrify(placeholder, x).decode("utf-8") for x in entities
+        )
+        return query_str
 
     def sql_command_execution(self, query: str, *value: tuple):
         try:
