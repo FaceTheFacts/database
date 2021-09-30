323b0b0f4e4c15d2247a6977c1835524fb3b9349
Kevin Gao <k_gao@aol.com>
2021-09-30 23:50:34 +0200

    refactor: many insert for politicians

--- src/models/politician.py
+++ src/models/politician.py
@@ -1,141 +1,84 @@
 import sys
-import json
 
 sys.path.append("src")
 from fetch import politician_fetch
 from query import Query
-
-columns = [
-    "id",
-    "entity_type",
-    "label",
-    "api_url",
-    "abgeordnetenwatch_url",
-    "first_name",
-    "last_name",
-    "birth_name",
-    "sex",
-    "year_of_birth",
-    "party_id",
-    "party_past",
-    "deceased",
-    "deceased_date",
-    "education",
-    "residence",
-    "occupation",
-    "statistic_questions",
-    "statistic_questions_answered",
-    "qid_wikidata",
-    "field_title",
-]
 
 
 class Politician:
     def __init__(self):
         self.new_query = Query()
         self.table_name = "politician"
+        self.columns = [
+            "id",
+            "entity_type",
+            "label",
+            "api_url",
+            "abgeordnetenwatch_url",
+            "first_name",
+            "last_name",
+            "birth_name",
+            "sex",
+            "year_of_birth",
+            "party_id",
+            "party_past",
+            "deceased",
+            "deceased_date",
+            "education",
+            "residence",
+            "occupation",
+            "statistic_questions",
+            "statistic_questions_answered",
+            "qid_wikidata",
+            "field_title",
+        ]
 
     def create_table(self):
-        sql_command = """CREATE TABLE {} (
-      {} integer PRIMARY KEY,
-      {} varchar,
-      {} varchar,
-      {} varchar,
-      {} varchar,
-      {} varchar,
-      {} varchar,
-      {} varchar,
-      {} varchar,
-      {} integer,
-      {} integer REFERENCES party(id),
-      {} varchar,
-      {} boolean,
-      {} date,
-      {} varchar,
-      {} varchar,
-      {} varchar,
-      {} varchar,
-      {} varchar,
-      {} varchar,
-      {} varchar
-    );""".format(
-            self.table_name, *columns
+        sql_command = """
+        CREATE TABLE {} (
+            {} integer PRIMARY KEY,
+            {} varchar,
+            {} varchar,
+            {} varchar,
+            {} varchar,
+            {} varchar,
+            {} varchar,
+            {} varchar,
+            {} varchar,
+            {} integer,
+            {} integer REFERENCES party(id),
+            {} varchar,
+            {} boolean,
+            {} date,
+            {} varchar,
+            {} varchar,
+            {} varchar,
+            {} varchar,
+            {} varchar,
+            {} varchar,
+            {} varchar
+        );""".format(
+            self.table_name, *self.columns
         )
 
         return self.new_query.sql_command_execution(sql_command)
 
-    def insert_data(self):
-        politicians = politician_fetch()
-        no_party_politicians = []
-        for politician in politicians:
-            id = politician.get("id")
-            party = politician.get("party")
-            sql_string = """INSERT INTO {}
-            (
-                {},
-                {},
-                {},
-                {},
-                {},
-                {},
-                {},
-                {},
-                {},
-                {},
-                {},
-                {},
-                {},
-                {},
-                {},
-                {},
-                {},
-                {},
-                {},
-                {},
-                {}
-            ) VALUES (
-                %s,
-                    %s,
-                    %s,
-                    %s,
-                    %s,
-                    %s,
-                    %s,
-                    %s,
-                    %s,
-                    %s,
-                    %s,
-                    %s,
-                    %s,
-                    %s,
-                    %s,
-                    %s,
-                    %s,
-                    %s,
-                    %s,
-                    %s,
-                    %s
+    def populate_table(self):
+        api_politicians = politician_fetch()
+        politician_list = []
+        api_fields = self.columns.copy()
+        api_fields[10] = "party"
 
-                ) ON CONFLICT(id) DO NOTHING""".format(
-                self.table_name,
-                *columns,
-            )
+        for api_politician in api_politicians:
+            party = api_politician["party"]
+            politician_values = list(map(lambda k: api_politician[k], api_fields))
+            politician_values[10] = api_politician["party"]["id"] if party else None
+            politician_list.append(tuple(politician_values))
 
-            politician_values = list(map(lambda k: politician[k], columns))
-
-            if party is None:
-                print("ID: No.{id} politician doesn't have a party".format(id=id))
-                politician_values[10] = politician["party"]
-            else:
-                politician_values[10] = politician["party"]["id"]
-
-            sql_tuple = tuple(politician_values)
-
-            self.new_query.sql_command_execution(sql_string, sql_tuple)
-
-        with open("no_party_politicians.json", "w", encoding="utf-8") as file:
-            json.dump(no_party_politicians, file, ensure_ascii=False, indent=4)
-        return
+        query_str = self.new_query.generate_query_str(politician_list)
+        args = ", ".join(self.columns)
+        query_cmd = f"INSERT INTO {self.table_name} ({args}) VALUES "
+        self.new_query.sql_command_execution(query_cmd + query_str)
 
     def cursor_close(self):
         return self.new_query.cursor_close()
@@ -146,6 +89,6 @@ class Politician:
 
 politician = Politician()
 politician.create_table()
-politician.insert_data()
+politician.populate_table()
 politician.cursor_close()
 politician.connection_close()
