5d6d9e8db7052d5d6b7490a7675dcf89709dfef6
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-10-01 14:54:12 +0200

    Add City model

--- src/models.py
+++ src/models.py
@@ -2,7 +2,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, Integer
 from sqlalchemy.orm import session
 from connection import Session, engine
-from fetch import country_fetch
+from fetch import country_fetch, city_fetch
 
 Base = declarative_base()
 session = Session()
@@ -31,7 +31,31 @@ def insert_country(data: list):
     session.close()
 
 
+class City(Base):
+    __tablename__ = "city"
+    id = Column(Integer(), primary_key=True)
+    entity_type = Column(String)
+    label = Column(String)
+    api_url = Column(String, unique=True)
+
+
+def insert_city(data: list):
+    data_list = []
+    for datum in data:
+        new_country = City(
+            id=datum["id"],
+            entity_type=datum["entity_type"],
+            label=datum["label"],
+            api_url=datum["api_url"],
+        )
+        data_list.append(new_country)
+    session.add_all(data_list)
+    session.commit()
+    session.close()
+
+
 if __name__ == "__main__":
     # Migration
     Base.metadata.create_all(engine)
     insert_country(country_fetch())
+    insert_city(city_fetch())
