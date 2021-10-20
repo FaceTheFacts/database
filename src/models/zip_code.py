5c01ec422d7504bb94c5d5e464672161e89defff
Richard <richard.kruemmel@code.berlin>
2021-10-20 19:21:32 +0200

    Change zip_code to being a string

--- src/models/zip_code.py
+++ src/models/zip_code.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 
 from ..db.base import Base
@@ -7,9 +7,9 @@ from ..db.base import Base
 class ZipCode(Base):
     __tablename__ = "zip_code"
 
-    id = Column(Integer, primary_key=True),
-    constituency_id = Column(Integer, ForeignKey("constituency.id")),
-    zip_code = Column(Integer)
+    id = Column(Integer, primary_key=True)
+    constituency_id = Column(Integer, ForeignKey("constituency.id"))
+    zip_code = Column(String)
 
     # Many to One
     constituency = relationship("Constituency", back_populates="zip_codes")
